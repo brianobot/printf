47dc3ff961d80566e134fa33e574fc4b06abe97c
Brian Obot <brianobot9@gmail.com>
2022-11-22 18:33:35 -0800

    Add new task

---
+++ _putchar.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string with a newline
+ * @str: the string to print
+ * Return: string a
+ */
+int _puts(char *str)
+{
+	char *a = str;
+
+	while (*str)
+		_putchar(*str++);
+	return (str - a);
+}
+
+/**
+ * _putchar - writes the char c to stdout
+ * @c: the char argument
+ *
+ * Return: on success 1, on error -1
+ */
+int _putchar(int c)
+{
+	static int i;
+	static char buf[OUTPUT_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= OUTPUT_BUF_SIZE)
+	{
+		write(1, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buff[i++] = c;
+	return (1);
+}
