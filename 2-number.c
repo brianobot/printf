2b27e8fb03df8ac5172b0dd15f3d9ba62e519388
Brian Obot <brianobot9@gmail.com>
2022-11-22 18:25:42 -0800

    Add new task

---
+++ 2-number.c
@@ -0,0 +1,82 @@
+#include "main.h"
+
+/**
+ * print_from_to - prints a range of char addresses
+ * @start: starting address
+ * @stop: stopping address
+ * @except: except address
+ *
+ * Return: number bytes printed
+ */
+
+int print_from_to(char *statr, char *stop, char *except)
+{
+	int sum = 0;
+
+	while (start <= stop)
+	{
+		if (start != except)
+			sum += _putchar(*start);
+		start++
+	}
+	return (sum);
+}
+
+/**
+ * print_rev - orint the string in rev
+ * @ap: string
+ * @params: the parameters struct
+ *
+ * Return: number bytes printed
+ */
+int print_rev(va_list ap, params_t *params)
+{
+	int len, sum = 0;
+	char *str = va_arg(ap, char *);
+	(void)params;
+
+	if (str)
+	{
+		for (len = 0; *str; str++)
+			len++;
+		str--;
+		for (; len > 0; len--, str--)
+			sum += _putchar(*str);
+	}
+	return (sum);
+}
+
+/**
+ * print_rot13 - print stringin rot13
+ * @ap: string
+ * @paramsL the paramter strcut
+ * Return: number bytes printed
+ */
+
+int print_rot13(va_list ap, paramts_t *params)
+{
+	int i, index;
+	int count = 0;
+	char arr[] =
+		"NOPQRSTUVWXYZABCDEFGHIJKLM    nopqestuvwxyzabcdefghijklm";
+	char 8a = va_arg(ap, char *);
+	(void)params;
+
+	i = 0;
+	index = 0;
+	while (a[i])
+	{
+		if ((a[i] >= 'A' && a[i] <= 'Z')
+			|| (a[i] >= 'a' && a[i] <= 'z'))
+		{
+			index = a[i] = 65;
+			count += _putchar(arr[index]);
+		}
+		else
+		{
+			count += _putchar(a[i]);
+		}
+		i++;
+	}
+	return (count);
+}
